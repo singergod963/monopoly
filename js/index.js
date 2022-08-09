0cdaca93e507509f7084504cfc01dc73c8b1585a
Simon Chang <singergod963@gmail.com>
2022-08-09 23:23:31 +0800

    完成遊戲套版 S16 <=> S18 & 問題9 <=> 問題10對調

--- js/index.js
+++ js/index.js
@@ -1,16 +1,43 @@
 $(function () {
+    const questionSteps = [1, 3, 6, 7, 9, 11, 12, 14, 18, 16];
+    const answers = [2, 1, 1, 4, 5, 1, 1, 1, 3, 4];
     let questionIndex = 0;
-    $('.js-go').on('click', function () {
-        $('.question').eq(questionIndex).show();
+
+    $('.link--go').on('click', function () {
+        goToNextStep(questionIndex);
     });
 
-    $('.question').on('click', function() {
-        $('.question').eq(questionIndex).hide();
+    function goToNextStep() {
         questionIndex++;
-        if(questionIndex < $('.question').length) {
-            $('.question').eq(questionIndex).show();
-        }else{
+        if(questionIndex > questionSteps.length){
             $('.success').show();
+            return;
         }
+
+        $('.step').removeClass('step--active');
+        $('.step--s' + questionSteps[questionIndex - 1]).addClass('step--active');
+        $('.question').hide();
+        $('.question--q' + questionIndex).css('display', 'flex');
+        $('.QA').fadeIn(500);
+    }
+
+    $('.optionsText-item').css('user-select', 'none');
+
+    $('.optionBtn-item, .optionText-item').on('click', function () {
+        const correct = answers[questionIndex - 1];
+        const answer = $(this).index();
+        if (correct === (answer + 1)) {
+            alert($('.question--q' + questionIndex).find('.optionText-item').eq(answer).text());
+            goToNextStep();
+        } else {
+            $('.fail').show();
+            $('.failAns').hide();
+            $('.failAns--q' + questionIndex).show();
+        }
+        
+    });
+
+    $('.link--back').on('click', function () {
+        $('.fail').fadeOut(500);
     })
 });
